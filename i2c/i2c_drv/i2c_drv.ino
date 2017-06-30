af65a53243be1e17dc9c2dc92441d1710c67e1a7
Alexey <asteriosrebys@yandex.ru>
2017-06-30 18:19:45 +0300

    arduino/i2c: delete tabs and whitespaces also add 		readme.txt file
    Signed-off-by: Alexey <asteriosrebys@yandex.ru>

--- i2c/i2c_drv/i2c_drv.ino
+++ i2c/i2c_drv/i2c_drv.ino
@@ -13,12 +13,12 @@
 #define SIZE_OFF         1
 
 struct i2c_reg {
-  	unsigned char twbr;
-  	unsigned char twsr;
-  	unsigned char twar;
-  	unsigned char twdr;
-  	unsigned char twcr;
-  	unsigned char twamr;
+	unsigned char twbr;
+	unsigned char twsr;
+	unsigned char twar;
+	unsigned char twdr;
+	unsigned char twcr;
+	unsigned char twamr;
 };
 
 volatile static struct i2c_reg *reg = (void *)I2C_BASE;
@@ -28,8 +28,8 @@ volatile static struct i2c_reg *reg = (void *)I2C_BASE;
  */
 void i2c_init()
 {
-  	reg->twbr = 0x48;
-  	reg->twsr = 0x00;
+	reg->twbr = 0x48;
+	reg->twsr = 0x00;
 }
 
 /*
@@ -37,51 +37,51 @@ void i2c_init()
  */
 int i2c_start (unsigned char addr)
 {
- 	/*  
-   	 * Clean register
-   	 */
-  	reg->twcr = 0;
-  
-  	/*  
-   	 * Send START condition
-   	 */
-  	reg->twcr = (1 << 7) | (1 << 5) | (1 << 2);
+	/*
+	 * Clean register
+	 */
+	reg->twcr = 0;
 
- 	/*
-    	 * Wait that START condition transmitted
-    	 */
-   	while (!(reg->twcr & (1 << 7)));
+	/*
+	 * Send START condition
+	 */
+	reg->twcr = (1 << 7) | (1 << 5) | (1 << 2);
 
-    	/*
-     	 * Check if we received ACK START
-     	 */
-     	if ((reg->twsr & 0xF8) != 0x08)
-      		      return 1;
+	/*
+	 * Wait that START condition transmitted
+	 */
+	while (!(reg->twcr & (1 << 7)));
 
-    	/*
-    	 * Load addr
-    	 */
-   	reg->twdr = addr;
-   
-   	reg->twcr |= (1 << 7) | (1 << 2);
+	/*
+	 * Check if we received ACK START
+	 */
+	if ((reg->twsr & 0xF8) != 0x08)
+		return 1;
 
-   	while (!(reg->twcr & (1 << 7)));
+	/*
+	 * Load addr
+	 */
+	reg->twdr = addr;
 
-   	if (((reg->twsr & 0xF8) != 0x18) && ((reg->twsr & 0xF8) != 0x40))
-            	return 2;
+	reg->twcr |= (1 << 7) | (1 << 2);
 
-    	return 0;
+	while (!(reg->twcr & (1 << 7)));
+
+	if (((reg->twsr & 0xF8) != 0x18) && ((reg->twsr & 0xF8) != 0x40))
+		return 2;
+
+	return 0;
 }
 
-/*  
+/*
  * Send STOP to I2C
  */
 void i2c_stop()
 {
-	/*  
-   	 * Send STOP condition
-   	 */
-   	reg->twcr = (1 << 7) | (1 << 4) | (1 << 2); 
+	/*
+	 * Send STOP condition
+	 */
+	reg->twcr = (1 << 7) | (1 << 4) | (1 << 2);
 }
 
 /*
@@ -92,22 +92,22 @@ int i2c_write(unsigned char buf)
 	/*
 	 * Write DATA
 	 */
-  	reg->twdr = buf;
+	reg->twdr = buf;
 
-   	reg->twcr = (1 << 7) | (1 << 2);
+	reg->twcr = (1 << 7) | (1 << 2);
 
-   	/*
-	   * Wait until it transmit
-	   */
-   	while (!(reg->twcr & (1 << 7)));
+	/*
+	 * Wait until it transmit
+	 */
+	while (!(reg->twcr & (1 << 7)));
 
-   	/*
+	/*
 	 * Check if we receive DATA ACK
 	 */
-   	if ((reg->twsr & 0xF8) != 0x28)
-      		return 1;
+	if ((reg->twsr & 0xF8) != 0x28)
+		return 1;
 
-   	return 0;
+	return 0;
 }
 
 /*
@@ -117,9 +117,9 @@ void i2c_read(unsigned char *buf)
 {
 	reg->twcr = (1 << 7) | (1 << 2);
 
-      	while (!(reg->twcr & (1 << 7)));
-      
-     	*buf = reg->twdr; 
+	while (!(reg->twcr & (1 << 7)));
+
+	*buf = reg->twdr;
 }
 
 int main()
---
+++ i2c/readme.txt
@@ -0,0 +1,3 @@
+Add i2c implementation.
+If you want to test it you need to have your own i2c device and
+include it inside while loop in main function.
